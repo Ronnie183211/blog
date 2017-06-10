8194ff8d5269fcadc658a5afaec6f94b03d98541
ronnie7 <ronnie.ren7@gmail.com>
2017-06-10 21:35:04 +1000

    added blog views files including master layouts and partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="blog-footer">
+      <p>Blog template built based on <a href="http://getbootstrap.com">Bootstrap</a> by <a href="">Ronnie Ren</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+</footer>
